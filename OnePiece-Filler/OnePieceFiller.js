0771b94851ec0e9a6366c3f34563a994d4eb46a0
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2024-10-13 01:46:36 +0200

    Create OnePieceFiller.js

---
+++ OnePiece-Filler/OnePieceFiller.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         AniWorld OnePice Filler highlighter
+// @description  Highlights Filler episodes on aniworld for OnePiece in red
+// @namespace    https://github.com/Johannes7k75
+// @version      1.0.1
+// @match        https://aniworld.to/anime/stream/one-piece/*
+// @updateURL    https://raw.githubusercontent.com/Johannes7k75/tapermonkey-user-scripts/refs/heads/main/OnePiece-Filler/OnePieceFiller.js
+// @downloadURL  https://raw.githubusercontent.com/Johannes7k75/tapermonkey-user-scripts/refs/heads/main/OnePiece-Filler/OnePieceFiller.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  
+})();
