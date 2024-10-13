808f9758935dbed74c206c8de9bf2e5695a92c48
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2024-10-13 20:55:09 +0200

    Update OnePieceFiller.js

--- OnePiece-Filler/OnePieceFiller.js
+++ OnePiece-Filler/OnePieceFiller.js
@@ -3,7 +3,7 @@
 // @description             Highlights Filler episodes on aniworld for OnePiece in red
 // @namespace               https://github.com/Johannes7k75
 // @version                 1.0.2
-// @match                   https://aniworld.to/anime/stream/one-piece/*
+// @match                   https://aniworld.to/anime/stream/one-piece*
 // @updateURL               https://raw.githubusercontent.com/Johannes7k75/tapermonkey-user-scripts/refs/heads/main/OnePiece-Filler/OnePieceFiller.js
 // @downloadURL             https://raw.githubusercontent.com/Johannes7k75/tapermonkey-user-scripts/refs/heads/main/OnePiece-Filler/OnePieceFiller.js
 // @resource one-piece.json https://github.com/Johannes7k75/tapermonkey-user-scripts/releases/download/OnePieceFiller/one-piece.json
